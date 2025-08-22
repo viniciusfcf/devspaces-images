2d1a0683ad2f4dbb7a2e1f8a83e1adc9f0da2c47
Vinicius Ferraz <vinicius.ferraz@gmail.com>
2025-08-22 19:54:51 -0300

    feat: update Azure CLI installation script to use YUM repository

--- modules/azure-cli/install.sh
+++ modules/azure-cli/install.sh
@@ -1,2 +1,16 @@
 #!/usr/bin/env bash
-curl -L https://aka.ms/InstallAzureCli | bash
+set -e
+
+# Import Microsoft GPG key
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+
+# Add Azure CLI repo
+echo -e "[azure-cli]
+name=Azure CLI
+baseurl=https://packages.microsoft.com/yumrepos/azure-cli
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo
+
+# Install Azure CLI
+dnf install -y azure-cli
