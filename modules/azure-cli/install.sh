a68452e6d2ea9b843f4b7638e13f3e6414da1f23
Vinicius Ferraz <vinicius.ferraz@gmail.com>
2025-08-22 22:04:43 -0300

    fix: update Python image version to 3.12 and improve Azure CLI installation script

--- modules/azure-cli/install.sh
+++ modules/azure-cli/install.sh
@@ -1,16 +1,19 @@
+
 #!/usr/bin/env bash
 set -e
 
 # Import Microsoft GPG key
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 
-# Add Azure CLI repo
-echo -e "[azure-cli]
+# Add Azure CLI repo for dnf/yum
+cat <<EOF > /etc/yum.repos.d/azure-cli.repo
+[azure-cli]
 name=Azure CLI
 baseurl=https://packages.microsoft.com/yumrepos/azure-cli
 enabled=1
 gpgcheck=1
-gpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+EOF
 
 # Install Azure CLI
 dnf install -y azure-cli
