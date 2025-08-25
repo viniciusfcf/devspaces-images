72fad4d7b36eccb3af42a7b3ea99ae8fbbe38f68
Vinicius Ferraz <vinicius.ferraz@gmail.com>
2025-08-25 13:56:28 -0300

    install ibmcloud. Install azure cli using pip

--- modules/azure-cli/install.sh
+++ modules/azure-cli/install.sh
@@ -1,19 +1,3 @@
 
 #!/usr/bin/env bash
-set -e
-
-# Import Microsoft GPG key
-rpm --import https://packages.microsoft.com/keys/microsoft.asc
-
-# Add Azure CLI repo for dnf/yum
-cat <<EOF > /etc/yum.repos.d/azure-cli.repo
-[azure-cli]
-name=Azure CLI
-baseurl=https://packages.microsoft.com/yumrepos/azure-cli
-enabled=1
-gpgcheck=1
-gpgkey=https://packages.microsoft.com/keys/microsoft.asc
-EOF
-
-# Install Azure CLI
-dnf install -y azure-cli
+pip install azure-cli
